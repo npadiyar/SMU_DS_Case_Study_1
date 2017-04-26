5a260a181f0d1d4850da08c0f0026c78a162a2b5
npadiyar <npadiyar@gmail.com>
2017-04-25 20:47:44 -0400

    Add files via upload

---
+++ Case_Study1_Q5.R
@@ -0,0 +1,9 @@
+quantile(sort_merged_V5_Y_NA_sub$V5.y, probs = seq(0, 1), na.rm=FALSE)
+
+quantile(sort_merged_V5_Y_NA_sub$GDPRanking, probs = seq(0, 1), na.rm=FALSE)
+
+quantile(sort_merged_GDP_NA_sub$GDPRanking, probs = seq(0, 1), na.rm=FALSE)
+
+quantile(data$fpkm, prob = seq(0, 1, length = 11), type = 5
+
+quantile(sort_merged_GDP_NA_sub$GDPRanking, prob = seq(0, 1, length=11), type=5
